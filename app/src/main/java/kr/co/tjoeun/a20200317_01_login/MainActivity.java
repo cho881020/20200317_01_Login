d05dc8ffd467b63c53ed4d025d9d6932f006832e
cho881020 <kj_cho@nepp.kr>
2020-03-17 12:43:06 +0900

    롱클릭 이벤트 별도 처리

--- app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
@@ -3,6 +3,7 @@ package kr.co.tjoeun.a20200317_01_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,6 +27,37 @@ public class MainActivity extends AppCompatActivity {
 //        findPwTxt 로 id를 부여하자.
 
         findPwTxt.setText("비번 찾기");
+
+//        로그인 버튼이 눌리면 => 비번 찾기 버튼을 "AAA"로 변경
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                findPwTxt.setText("AAA");
+
+            }
+        });
+
+//        클릭은 버튼 뿐 아니라 모든 뷰가 다 눌릴 수 있다. => 텍스트뷰도 클릭.
+
+        findPwTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPwTxt.setText("비밀번호 찾기");
+            }
+        });
+
+
+        loginBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                loginBtn.setText("로그인");
+                return true; // 손을 뗐을때, onClick실행을 막을건지?
+            }
+        });
+
+//
 
     }
 }
